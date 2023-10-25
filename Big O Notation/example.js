591bef07cbf1f3ec2a410caf408ad93a9d9762c5
rakshith_s <rakshithsantosh@gmail.com>
2023-10-25 12:02:27 +0530

    big O of objects and arrays

--- Big O Notation/example.js
+++ Big O Notation/example.js
@@ -21,3 +21,21 @@ function addition(num){
 
 //arithmetic operations variable assignments and accessing elements in an array or object are constant
 
+//Big O of objects
+
+let instructor = {
+    name : "rakshith",
+    lastName : "s",
+    favoriteNumbers:[1,2,3,4]
+}
+
+// insertion,removal and access O(1) for searching its O(n)
+// for objects methods like Obj.keys Obj.values Obj.entries its O(n) and for hasOwnPropert its O(1)
+
+//Big O of array operations
+
+/*push pop O(1)
+shift unshift O(n)
+concat splice slice O(n)
+sort O(N*LOGN)
+for each /map/filter/reduce O(n)*/
